d53f046ef24cdee22cea6190eb241cf7ed0caaa4
Max Metral <max@pyralis.com>
2017-07-26 22:36:22 -0400

    Try setting env vars from a step

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export NODE_ENV=production
+export AWS_ECR_TAG=$(cat WERCKER_BUILD_ID)_$WERCKER_GIT_COMMIT
+
 rm -rf node_modules/.bin
 npm prune --production
 npm rebuild $WERCKER_PREPARE_GASBUDDY_DEPLOY_REBUILD
