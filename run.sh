909a7adea8cd4a154ecd0a8db5e28feb4d6b828e
Max Metral <max@pyralis.com>
2018-05-01 16:42:29 -0400

    Remove .git dir in deployments

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ export AWS_ECR_TAG=$(cat WERCKER_BUILD_ID)_$WERCKER_GIT_COMMIT
 rm -rf node_modules/.bin
 npm prune --production
 npm rebuild $WERCKER_PREPARE_GASBUDDY_DEPLOY_REBUILD
-rm -rf ~/.npmrc src tests coverage .nyc_output /pipeline/cache config/development.json
+rm -rf ~/.npmrc src tests coverage .nyc_output /pipeline/cache config/development.json .git
