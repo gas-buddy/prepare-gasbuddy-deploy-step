01bf2fef94558597d73465aeadd7e7d45df7f24e
Max Metral <max@pyralis.com>
2018-11-02 12:00:22 -0400

    Fix bcrypt

--- run.sh
+++ run.sh
@@ -7,3 +7,10 @@ rm -rf node_modules/.bin
 npm prune --production
 npm rebuild $WERCKER_PREPARE_GASBUDDY_DEPLOY_REBUILD
 rm -rf ~/.npmrc src tests coverage .nyc_output /pipeline/cache config/development.json .git
+
+# Rebuild stupid bcrypt
+if npm ls bcrypt &>/dev/null; then
+  apk -q --no-progress --no-cache --virtual .bcryptdeps add make gcc g++ python
+  npm rebuild bcrypt --build-from-source=bcrypt
+  apk -q --no-progress --no-cache del .bcryptdeps
+fi
